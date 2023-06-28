f42935c314560c77953b9af4ad3db0426b9c6438
Sadraque Cuellar <sadraque.cuellar2001@gmail.com>
2023-06-28 11:59:28 -0400

    feat: new migrations

--- api/serverless.js
+++ api/serverless.js
@@ -13,7 +13,7 @@ const app = Fastify({
 });
 
 // Register your application as a normal plugin.
-app.register(import("../src/routes.ts"));
+app.register(import("../src/routes"));
 
 export default async (req, res) => {
     await app.ready();
--- prisma/migrations/20230211170332_created_model_task/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "task" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "color" TEXT NOT NULL,
-    "done" BOOLEAN NOT NULL
-);
