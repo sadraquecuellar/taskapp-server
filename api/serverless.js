c1880824bedc99808c1220297eb5121a15dede08
Sadraque Cuellar <sadraque.cuellar2001@gmail.com>
2023-06-28 12:17:53 -0400

    test

--- api/serverless.js
+++ api/serverless.js
@@ -1,19 +1,14 @@
 "use strict";
 
-// Read the .env file.
-// import * as dotenv from "dotenv";
-// dotenv.config();
-
-// Require the framework
 import Fastify from "fastify";
 
-// Instantiate Fastify with some config
+import { appRoutes } from "./routes";
+
 const app = Fastify({
   logger: true,
 });
 
-// Register your application as a normal plugin.
-app.register(import("../src/server.ts"));
+app.register(appRoutes);
 
 export default async (req, res) => {
     await app.ready();
